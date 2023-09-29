981f6f90b8e3ff22e1580af55d829f534e60ec27
Alejandro Rivas García <alejandrorivasgarcia@gmail.com>
2023-09-29 17:48:05 +0200

    Prueba version

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(name="TimeFeatures",
       packages=["timefeatures.widgets"],
       package_data={"timefeatures.widgets": ["icons/*.svg", "icons/*.png"]},
       entry_points={"orange.widgets": "Time-Features = timefeatures.widgets"},
-      version="0.0.1",
+      version="0.0.2",
       )
