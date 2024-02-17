e5ef3b6310f2e9580aa94c5a9f6b11f9fa28ba75
alervgr <alejandrorivasgarcia@gmail.com>
2024-02-17 18:50:41 +0100

    GUARDAR EN BD FUNCIONA

--- timefeatures/widgets/OWTimeFeatureDB.py
+++ timefeatures/widgets/OWTimeFeatureDB.py
@@ -1,6 +1,8 @@
+import Orange
 from AnyQt.QtWidgets import QComboBox, QTextEdit, QMessageBox, QApplication
 from AnyQt.QtGui import QCursor
 from AnyQt.QtCore import Qt
+from datetime import datetime
 
 from Orange.data import Table
 from Orange.data.sql.backend import Backend
@@ -10,8 +12,11 @@ from Orange.widgets import gui
 from Orange.widgets.settings import Setting
 from Orange.widgets.utils.itemmodels import PyListModel
 from Orange.widgets.utils.owbasesql import OWBaseSql
+from Orange.widgets.utils.sql import check_sql_input
 from Orange.widgets.utils.widgetpreview import WidgetPreview
 from Orange.widgets.widget import Output, Msg
+from PyQt5.QtWidgets import QGridLayout, QLineEdit, QPushButton, QSizePolicy, QLabel
+from orangewidget.utils.signals import Input
 
 MAX_DL_LIMIT = 1000000
 
@@ -43,8 +48,8 @@ class OWTimeFeatureDB(OWBaseSql):
     priority = 2240
     keywords = "sql table, save, data"
 
-    class Outputs:
-        data = Output("Data", Table, doc="Attribute-valued dataset read from the input file.")
+    class Inputs:
+        data = Input("Data", Orange.data.Table)
 
     settings_version = 2
 
@@ -67,6 +72,7 @@ class OWTimeFeatureDB(OWBaseSql):
 
     class Error(OWBaseSql.Error):
         no_backends = Msg("Please install a backend to use this widget.")
+        same_name = Msg("There is an instance with the same name already.")
 
     def __init__(self):
         # Lint
@@ -76,13 +82,56 @@ class OWTimeFeatureDB(OWBaseSql):
         self.tablecombo = None
         self.sqltext = None
         self.custom_sql = None
-        self.downloadcb = None
+        self.data = None
+        self.rows = 0
+        self.cols = 0
+        self.target = None
         super().__init__()
+
+    def update_labels(self):
+        self.target_label.setText("Class: " + str(self.target))
+        self.rows_label.setText("Rows: " + str(self.rows))
+        self.cols_label.setText("Columns: " + str(self.cols))
+
+    @Inputs.data
+    @check_sql_input
+    def setData(self, data=None):
+
+        self.data = data
+        self.btn_savedata.setEnabled(bool(self.data))
+        self.rows = len(self.data)
+        self.cols = len(self.data.domain)
+        target_variable = self.data.domain.class_var
+        if target_variable is not None:
+            if isinstance(target_variable, Orange.data.DiscreteVariable):
+                self.target = "categorical"
+            if isinstance(target_variable, Orange.data.ContinuousVariable):
+                self.target = "numeric"
+        else:
+            self.target = None
+        self.update_labels()
 
     def _setup_gui(self):
         super()._setup_gui()
+        layoutA = QGridLayout()
+        layoutA.setSpacing(1)
+        gui.widgetBox(self.controlArea, orientation=layoutA, box='Save dataset')
+        self.target_label = QLabel()
+        layoutA.addWidget(self.target_label)
+        self.rows_label = QLabel()
+        layoutA.addWidget(self.rows_label)
+        self.cols_label = QLabel()
+        layoutA.addWidget(self.cols_label)
+        self.tableName = QLineEdit(
+            placeholderText="Table name...", toolTip="Table name")
+        layoutA.addWidget(self.tableName)
+        self.btn_savedata = QPushButton(
+            "Save", toolTip="Save a dataset into a DB",
+            minimumWidth=120
+        )
+        self.btn_savedata.clicked.connect(self.saveData)
+        layoutA.addWidget(self.btn_savedata)
         self._add_backend_controls()
-        self._add_tables_controls()
 
     def _add_backend_controls(self):
         box = self.serverbox
@@ -103,43 +152,16 @@ class OWTimeFeatureDB(OWBaseSql):
         backend = self.get_backend()
         self.selected_backend = backend.display_name if backend else None
 
-    def _add_tables_controls(self):
-        vbox = gui.vBox(self.controlArea, "Tables")
-        box = gui.vBox(vbox)
-        self.tables = TableModel()
+    def saveData(self):
 
-        self.tablecombo = QComboBox(
-            minimumContentsLength=35,
-            sizeAdjustPolicy=QComboBox.AdjustToMinimumContentsLengthWithIcon
-        )
-        self.tablecombo.setModel(self.tables)
-        self.tablecombo.setToolTip('table')
-        self.tablecombo.activated[int].connect(self.select_table)
-        box.layout().addWidget(self.tablecombo)
+        query = "INSERT INTO public.datasets (name, datetime, rows, cols, class) VALUES ('" + self.tableName.text() + "','" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "','" + str(self.rows) + "','" + str(self.cols) + "','" + str(self.target) + "'); "
 
-        self.custom_sql = gui.vBox(box)
-        self.custom_sql.setVisible(False)
-        self.sqltext = QTextEdit(self.custom_sql)
-        self.sqltext.setPlainText(self.sql)
-        self.custom_sql.layout().addWidget(self.sqltext)
-
-        mt = gui.hBox(self.custom_sql)
-        cb = gui.checkBox(mt, self, 'materialize', 'Materialize to table ')
-        cb.setToolTip('Save results of the query in a table')
-        le = gui.lineEdit(mt, self, 'materialize_table_name')
-        le.setToolTip('Save results of the query in a table')
-
-        gui.button(self.custom_sql, self, 'Execute', callback=self.open_table)
-
-        box.layout().addWidget(self.custom_sql)
-
-        gui.checkBox(box, self, "guess_values",
-                     "Auto-discover categorical variables",
-                     callback=self.open_table)
-
-        self.downloadcb = gui.checkBox(box, self, "download",
-                                       "Download data to local memory",
-                                       callback=self.open_table)
+        try:
+            with self.backend.execute_sql_query(query):
+                pass
+        except BackendError as ex:
+            self.Error.same_name()
+            print("Error inserting data:", ex)
 
     def highlight_error(self, text=""):
         err = ['', 'QLineEdit {border: 2px solid red;}']
@@ -153,17 +175,7 @@ class OWTimeFeatureDB(OWBaseSql):
         return self.backends[self.backendcombo.currentIndex()]
 
     def on_connection_success(self):
-        if getattr(self.backend, 'missing_extension', False):
-            self.Warning.missing_extension(
-                ", ".join(self.backend.missing_extension))
-            self.download = True
-            self.downloadcb.setEnabled(False)
-        if not is_postgres(self.backend):
-            self.download = True
-            self.downloadcb.setEnabled(False)
         super().on_connection_success()
-        self.refresh_tables()
-        self.select_table()
 
     def on_connection_error(self, err):
         super().on_connection_error(err)
@@ -172,163 +184,7 @@ class OWTimeFeatureDB(OWBaseSql):
     def clear(self):
         super().clear()
         self.Warning.missing_extension.clear()
-        self.downloadcb.setEnabled(True)
         self.highlight_error()
-        self.tablecombo.clear()
-        self.tablecombo.repaint()
-
-    def refresh_tables(self):
-        self.tables.clear()
-        if self.backend is None:
-            self.data_desc_table = None
-            return
-
-        self.tables.append("Select a table")
-        self.tables.append("Custom SQL")
-        self.tables.extend(self.backend.list_tables(self.schema))
-        index = self.tablecombo.findText(str(self.table))
-        self.tablecombo.setCurrentIndex(index if index != -1 else 0)
-        self.tablecombo.repaint()
-
-    # Called on tablecombo selection change:
-    def select_table(self):
-        curIdx = self.tablecombo.currentIndex()
-        if self.tablecombo.itemText(curIdx) != "Custom SQL":
-            self.custom_sql.setVisible(False)
-            return self.open_table()
-        else:
-            self.custom_sql.setVisible(True)
-            self.data_desc_table = None
-            self.database_desc["Table"] = "(None)"
-            self.table = None
-            if len(str(self.sql)) > 14:
-                return self.open_table()
-        return None
-
-    def get_table(self):
-        curIdx = self.tablecombo.currentIndex()
-        if curIdx <= 0:
-            if self.database_desc:
-                self.database_desc["Table"] = "(None)"
-            self.data_desc_table = None
-            return None
-
-        if self.tablecombo.itemText(curIdx) != "Custom SQL":
-            self.table = self.tables[self.tablecombo.currentIndex()]
-            self.database_desc["Table"] = self.table
-            if "Query" in self.database_desc:
-                del self.database_desc["Query"]
-            what = self.table
-        else:
-            what = self.sql = self.sqltext.toPlainText()
-            self.table = "Custom SQL"
-            if self.materialize:
-                if not self.materialize_table_name:
-                    self.Error.connection(
-                        "Specify a table name to materialize the query")
-                    return None
-                try:
-                    with self.backend.execute_sql_query("DROP TABLE IF EXISTS " +
-                                                        self.materialize_table_name):
-                        pass
-                    with self.backend.execute_sql_query("CREATE TABLE " +
-                                                        self.materialize_table_name +
-                                                        " AS " + self.sql):
-                        pass
-                    with self.backend.execute_sql_query("ANALYZE " + self.materialize_table_name):
-                        pass
-                except BackendError as ex:
-                    self.Error.connection(str(ex))
-                    return None
-
-        try:
-            table = SqlTable(dict(host=self.host,
-                                  port=self.port,
-                                  database=self.database,
-                                  user=self.username,
-                                  password=self.password),
-                             what,
-                             backend=type(self.backend),
-                             inspect_values=False)
-        except BackendError as ex:
-            self.Error.connection(str(ex))
-            return None
-
-        self.Error.connection.clear()
-
-        sample = False
-
-        if table.approx_len() > LARGE_TABLE and self.guess_values:
-            confirm = QMessageBox(self)
-            confirm.setIcon(QMessageBox.Warning)
-            confirm.setText("Attribute discovery might take "
-                            "a long time on large tables.\n"
-                            "Do you want to auto discover attributes?")
-            confirm.addButton("Yes", QMessageBox.YesRole)
-            no_button = confirm.addButton("No", QMessageBox.NoRole)
-            if is_postgres(self.backend):
-                sample_button = confirm.addButton("Yes, on a sample",
-                                                  QMessageBox.YesRole)
-            confirm.exec()
-            if confirm.clickedButton() == no_button:
-                self.guess_values = False
-            elif is_postgres(self.backend) and \
-                    confirm.clickedButton() == sample_button:
-                sample = True
-
-        self.Information.clear()
-        if self.guess_values:
-            QApplication.setOverrideCursor(QCursor(Qt.WaitCursor))
-            if sample:
-                s = table.sample_time(1)
-                domain = s.get_domain(inspect_values=True)
-                self.Information.data_sampled()
-            else:
-                domain = table.get_domain(inspect_values=True)
-            QApplication.restoreOverrideCursor()
-            table.domain = domain
-
-        if self.download:
-            if table.approx_len() > AUTO_DL_LIMIT:
-                if is_postgres(self.backend):
-                    confirm = QMessageBox(self)
-                    confirm.setIcon(QMessageBox.Warning)
-                    confirm.setText("Data appears to be big. Do you really "
-                                    "want to download it to local memory?\n"
-                                    "Table length: {:,}. Limit {:,}".format(table.approx_len(), MAX_DL_LIMIT))
-
-                    if table.approx_len() <= MAX_DL_LIMIT:
-                        confirm.addButton("Yes", QMessageBox.YesRole)
-                    no_button = confirm.addButton("No", QMessageBox.NoRole)
-                    sample_button = confirm.addButton("Yes, a sample",
-                                                      QMessageBox.YesRole)
-                    confirm.exec()
-                    if confirm.clickedButton() == no_button:
-                        return None
-                    elif confirm.clickedButton() == sample_button:
-                        table = table.sample_percentage(
-                            AUTO_DL_LIMIT / table.approx_len() * 100)
-                else:
-                    if table.approx_len() > MAX_DL_LIMIT:
-                        QMessageBox.warning(
-                            self, 'Warning',
-                            "Data is too big to download.\n"
-                            "Table length: {:,}. Limit {:,}".format(table.approx_len(), MAX_DL_LIMIT)
-                        )
-                        return None
-                    else:
-                        confirm = QMessageBox.question(
-                            self, 'Question',
-                            "Data appears to be big. Do you really "
-                            "want to download it to local memory?",
-                            QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
-                        if confirm == QMessageBox.No:
-                            return None
-
-            table.download_data(MAX_DL_LIMIT)
-            table = Table(table)
-
-        return table
 
     @classmethod
     def migrate_settings(cls, settings, version):
