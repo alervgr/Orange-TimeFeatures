5e97598bd83119f9f8f441fa8f870fa5c36a2f5c
alervgr <alejandrorivasgarcia@gmail.com>
2024-02-23 03:05:51 +0100

    Empaquetado

--- setup.py
+++ setup.py
@@ -5,5 +5,10 @@ setup(name="TimeFeatures",
       package_data={"timefeatures.widgets": ["icons/*.svg", "icons/*.png"]},
       entry_points={"orange.widgets": "Time-Features = timefeatures.widgets"},
       version="1.0.0",
-      author="Alejandro Rivas Garcia"
+      author="Alejandro Rivas García"
+      author_email="alejandrorivasgarcia@gmail.com"
+      keywords=[
+    'orange3 add-on','timefeatures','graph','time series'
+]
+      url=https://github.com/alervgr/Orange-TimeFeatures
       )
