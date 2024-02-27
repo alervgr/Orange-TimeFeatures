bcfcf6eaa8aac4594ebc92a51ff1381b32e114b1
alervgr <alejandrorivasgarcia@gmail.com>
2024-02-27 03:18:28 +0100

    v 1.0.10

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(name="TimeFeatures",
       packages=["timefeatures.widgets"],
       package_data={"timefeatures.widgets": ["icons/*.svg", "icons/*.png"]},
       entry_points={"orange.widgets": "Time-Features = timefeatures.widgets"},
-      version="1.0.9",
+      version="1.0.10",
       author="Alejandro Rivas García",
       author_email="alejandrorivasgarcia@gmail.com",
       keywords=[
